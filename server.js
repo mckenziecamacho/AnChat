4406c5a0532c946b70048ef7451b092596fa9a37
Skills Academy <student03@MckenzieCamacho.fios-router.home>
2022-05-27 12:07:29 -0400

    errors resolved

--- server.js
+++ server.js
@@ -4,16 +4,10 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-const cors = require('cors');
 
-const socketio = require("socket.io")(server, {
-  cors: {
-    origin: "http://localhost:3005",
-    methods: ["GET", "POST"],
-    allowedHeaders: ["my-custom-header"],
-    credentials: true
-  }
-});
+const cors = require('cors');
+app.use(cors())
+
 
 io.on('connection', (socket) => {
   console.log('a user connected');
